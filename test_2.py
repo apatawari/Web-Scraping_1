1be27f18db55f3140e1d1efcd40a778e58eae036
KillJoy <aaronthegreat109@gmail.com>
2020-01-06 14:49:54 +0530

    idk man

--- test_2.py
+++ test_2.py
@@ -4,4 +4,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.common.exceptions import TimeoutException
 
+option = webdriver.ChromeOptions()
+option.add_argument(" — incognito")
+
+browser = webdriver.Chrome(executable_path=r"C:\Users\admin\Desktop\chromedriver", chrome_options=option)
+browser.get("https://github.com/TheDancerCodes")
+
+# Wait 30 seconds for page to load
+timeout = 30
+try:
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full rounded-2']")))
+except TimeoutException:
+    print("Timed out waiting for page to load")
+    browser.quit()
+
+# find_elements_by_xpath returns an array of selenium objects.
+titles_element = browser.find_elements_by_xpath(“//a[@class=’text-bold’]”)
+# use list comprehension to get the actual repo titles and not the selenium objects.
+titles = [x.text for x in titles_element]
+# print out all the titles.
+print('titles:')
+print(titles, '\n')    
+
+
+
 
