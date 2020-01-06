7421daf3aa00f9d019da32c5352ae61ea267266f
KillJoy <aaronthegreat109@gmail.com>
2020-01-06 14:37:51 +0530

    import

--- test_2.py
+++ test_2.py
@@ -1,5 +1,7 @@
-import selenium 
-from bs4 import BeautifulSoup
+from selenium import webdriver 
+from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import WebDriverWait 
+from selenium.webdriver.support import expected_conditions as EC 
+from selenium.common.exceptions import TimeoutException
 
-print('tf')
-print("Okay")
+
